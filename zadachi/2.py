fa0d61709700dda0cc87538eb7ef61ea9b9451e0
gfDark <darkdno0@gmail.com>
2018-08-20 16:03:21 +0300

    second

---
+++ zadachi/2.py
@@ -0,0 +1,18 @@
+#Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:43:06) [MSC v.1600 32 bit (Intel)] on win32
+#Type "copyright", "credits" or "license()" for more information.
+
+lst = [1,2,3,4,1,2,7,4,11,8]
+print(Counter(lst))
+for item in lst:
+        if item > 1:
+print(lst)
+
+#lst = [1,2,3,4,1,2,7,4,11,8]
+#output = [lst]
+#for x in trends:
+#    if x not in output:
+#        output.append(x)
+#print output
+
+
+go on ^)
---
+++ zadachi/4124.py
@@ -0,0 +1,3 @@
+lst = ["odesSa", 1467, "dnePr", 21, "Kharkov", 779, 14, "chernigoV", 82, "roVno"]
+lst = sorted(lst)
+print(lst)
